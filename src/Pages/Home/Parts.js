3ccc49eb803cf6c11882dfe64f31aec9b5fc94e4
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-26 19:30:46 +0600

    edit home parts page

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -43,12 +43,9 @@ const Parts = () => {
         <span className="text-5xl ">Products</span>
       </h2>
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  ">
-        {partsData
-          .slice(0, 6)
-          .reverse()
-          .map((partData) => (
-            <PartsData key={partData} partData={partData}></PartsData>
-          ))}
+        {partsData.map((partData) => (
+          <PartsData key={partData} partData={partData}></PartsData>
+        ))}
       </div>
     </div>
   );
