171ba6810f0418d08b2ea30345944325b9d69cc1
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-26 19:28:42 +0600

    edit home parts page

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
 import PartsData from "./PartsData";
 
 const Parts = () => {
-  const {
-    isLoading,
-    error,
-    data: partsData,
-    refetch,
-  } = useQuery("partsData", () =>
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: partsData,
+  //   refetch,
+  // } = useQuery("partsData", () =>
+  //   fetch("https://stark-cliffs-55109.herokuapp.com/parts", {
+  //     method: "GET",
+  //     headers: {
+  //       authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  //     },
+  //   }).then((res) => {
+  //     console.log("55", res);
+  //     return res.json();
+  //   })
+  // );
+
+  // console.log(partsData);
+  // if (isLoading) {
+  //   return <Loading></Loading>;
+  // }
+
+  const [partsData, setPartsData] = useState([]);
+  useEffect(() => {
     fetch("https://stark-cliffs-55109.herokuapp.com/parts", {
       method: "GET",
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
-    }).then((res) => {
-      console.log("55", res);
-      return res.json();
     })
-  );
-
-  console.log(partsData);
-  if (isLoading) {
-    return <Loading></Loading>;
-  }
+      .then((res) => res.json())
+      .then((data) => setPartsData(data));
+  }, []);
   return (
     <div className="my-20 ">
       <h2 className="uppercase pl-8 py-4 my-16 text-gray-100 bg-gradient-to-r from-cyan-500 to-blue-500">
